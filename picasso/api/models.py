ec285aba7f522e42320dc74f6729111ca730a8ad
Alexey Inkov <alex0982@mail.ru>
2024-01-31 22:22:39 +0300

    fix model

--- picasso/api/models.py
+++ picasso/api/models.py
@@ -5,6 +5,6 @@ class File(models.Model):
     """
     Модель File
     """
-    file = models.FileField()
+    file = models.FileField(upload_to='./files/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     processed = models.BooleanField(default=False)
